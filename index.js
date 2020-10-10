cb871ee02a6f599fe5ba38973845ab84fb17ebaf
abdisamad100 <abdisamadcade004@gmail.com>
2020-10-10 18:49:45 +0300

    commit all the changes

---
+++ index.js
@@ -0,0 +1,13 @@
+$("img").click(function(){
+    $(".des").toggle();
+  });
+  
+  $("img").click(function(){
+    $(".dev").toggle();
+  });
+
+  $("img").click(function(){
+    $(".hidden").toggle();
+  });
+
+  
