a7cae4a6d4f93ad671bf62c513dfb50d3a1a5e9c
abdisamad100 <abdisamadcade004@gmail.com>
2020-10-11 23:32:19 +0300

    commit all changes

--- index.js
+++ index.js
@@ -72,22 +72,19 @@ $("#work4")
 
   //form validation
   $("form#formInput").submit(function(event){
-    $("form#formInput").submit(function (event) {
+
         let name = $("input#name").val();
         let email = $("input#email").val();
-        let subject = $("textarea#subject").val();
-    
+        let subject = $("textarea#subject");
+
         if ($("input#name").val() && $("input#email").val()){
           alert (name + ", we have received your message. Thank you for reaching out to us.");
-        if ($("input#name").val() && $("input#email").val()) {
-            alert(name + ", we have received your message. Thank you for reaching out to us.");
         }
+
         else {
           alert("Please enter your name and email!");
-            alert("Please enter your name and email!");
-        }
-    
+        };
+
         event.preventDefault();
-      }
-    })
-  }) 
+      })
+  
