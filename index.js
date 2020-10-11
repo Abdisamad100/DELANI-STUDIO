92894b33f3dbe564f80cf0341c431f7c9d3f3815
abdisamad100 <abdisamadcade004@gmail.com>
2020-10-11 15:26:48 +0300

    commit all changes

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
-$(document).ready(function(){
-  $(".image").click(function(){
-    $(".des").hide();
-    $(".des") .show();
-  });
+$(".image").click(function(){
+  $(".hidden").toggle().slideUp(1000) .slideDown(1000);
 });
 
+//overlay 4
+$("#work4")
+  .mouseenter(function() {
+    $("#overlay4").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay4").hide("slow");
+  });
+
+//overlay3
+  $("#work3")
+  .mouseenter(function() {
+    $("#overlay3").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay3").hide("slow");
+  });
+//overlay2
+  $("#work2")
+  .mouseenter(function() {
+    $("#overlay2").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay2").hide("slow");
+  });
+  //overlay5
+  $("#work5")
+  .mouseenter(function() {
+    $("#overlay5").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay5").hide("slow");
+  });
+  //overlay6
+  $("#work6")
+  .mouseenter(function() {
+    $("#overlay6").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay6").hide("slow");
+  });
+  //overlay7
+  $("#work7")
+  .mouseenter(function() {
+    $("#overlay7").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay7").hide("slow");
+  });
+  //overlay8
+  $("#work8")
+  .mouseenter(function() {
+    $("#overlay8").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay8").hide("slow");
+  });
+  //overlay1
+  $("#work1")
+  .mouseenter(function() {
+    $("#overlay-1").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay-1").hide("slow");
+  });
 
 
